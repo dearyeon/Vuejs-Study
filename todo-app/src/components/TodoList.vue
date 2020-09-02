d27f565d178acbeba0dd68a8de1fb79f90205b3f
dearyeon <d_earyeon@naver.com>
2020-09-02 17:38:52 +0900

    publishing

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="list">
+        <li class="list_item">
+            <input type="checkbox" id="list-item-l" />
+            <label for="list-item-l">
+                <p class="list_text">Smile! :)</p>
+            </label>
+            <p class="list_date">5/26</p>
+            <button class="list_delete">Delete</button>
+        </li>
+    </ul>
+</template>
+
+<style lang="scss">
+$color: #f0f0f0;
+.list {
+  background: $color;
+
+  &__text {
+    font-weight: bold;
+  }
+}
+</style>
---
+++ todo-app/src/components/TodoTitle.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="title">
+        <p class="title_message">{{ message }}</p>
+        <p class="title_task">
+            You've got
+            <span class="title_task-total">{{ taskTotal }}</span> tasks today.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello, YeonJi.",
+            taskTotal: 5
+        };
+    }
+}
+</script>
+
+<style lang="scss">
+.title {
+  max-width: 720px;
+  margin: 0 auto;
+  letter-spacing: 0.03rem;
+  color: #fff;
+
+  &_message {
+    font-size: 1.6rem;
+  }
+
+  &_task {
+    margin-top: 3.5rem;
+    margin-bottom: 5rem;
+    font-weight: bold;
+
+    &-top {
+      display: block;
+      font-size: 2.6rem;
+    }
+
+    &-total {
+      font-size: 5.4rem;
+      line-height: 110%;
+    }
+  }
+}
+</style>
