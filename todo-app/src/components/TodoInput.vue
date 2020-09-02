4dcb096eddcc2b9b78b2e28b699353c06acb2eef
dearyeon <d_earyeon@naver.com>
2020-09-02 14:13:59 +0900

    markup

---
+++ todo-app/src/components/TodoInput.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="add">
+        <input type="text" class="add_input" placeholder="Enter your task" />
+        <button class="add_button">Add</button>
+    </div>
+</template>
---
+++ todo-app/src/components/TodoTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="title">
+        <p class="title_message">{{ message }}</p>
+        <p class="title_task">
+            You've got
+            <span class="title_task-total">{{ taskTotal }}</span> tasks today.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello, YeonJi.",
+            taskTotal: 5
+        };
+    }
+}
+</script>
